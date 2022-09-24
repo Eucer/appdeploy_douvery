723365267eff59aaebf4dabfa54fbd2a331517a5
Eucer <100469001+Eucer@users.noreply.github.com>
2022-09-24 13:37:57 -0400

    cdcdc

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:copy/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test cccccc',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
