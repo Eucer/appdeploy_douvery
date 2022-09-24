a7d97c354a1c852ca4e0ead8428f49a3bd1d8a7f
Eucer <100469001+Eucer@users.noreply.github.com>
2022-09-24 13:31:15 -0400

    ccc

--- lib/constantes/global_variables.dart
+++ lib/constantes/global_variables.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:v1douvery/provider/theme.dart';
 
-String uri = '/';
+String uri = '';
 
 class GlobalVariables {
   //* COLORES
